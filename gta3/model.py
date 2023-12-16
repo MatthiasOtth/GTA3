98a24d3240384fb115c8bdd065d125e9d57047e1
Matthias Otth <matthias.otth@gmail.com>
2023-12-16 12:12:02 +0100

    Batching model

--- gta3/model.py
+++ gta3/model.py
@@ -19,7 +19,7 @@ def phi_inverse_hops(a, A, alpha):
         new_a = torch.where(A==1, a, torch.zeros_like(a))
     else:
         x = torch.clamp(1/torch.abs(alpha), max=10)
-        new_a = 1./torch.pow(A, x) * a
+        new_a = torch.where(A==0, torch.zeros_like(a), 1./torch.pow(A, x) * a)
     new_a = F.normalize(new_a, p=1, dim=-1)
     # new_a = F.softmax(new_a, dim=-1)
 
@@ -84,8 +84,85 @@ class AdjacencyAwareMultiHeadAttention(nn.Module):
         # apply scaling and softmax to attention
         attention = self.softmax(attention / self.sqrt_out_dim)
 
+        
         # reweight using the adjacency information
         attention = self.phi(attention.transpose(-1,-2), A, alpha)
+        # sum value tensors scaled by the attention weights
+        h_heads = torch.matmul(attention, V_h)
+
+        return h_heads
+
+
+class AdjacencyAwareMultiHeadAttentionBatched(nn.Module):
+
+    def __init__(self, in_dim, out_dim, phi, num_heads=8, bias=True):
+        """
+        Adjacency aware multi head attention layer.
+
+        Args:
+          in_dim:    the dimension of the input vectors
+          out_dim:   the dimension of the output vectors
+          phi:       the weighting function to be used
+          num_heads: the number of attention heads to be used (default: 8)
+          bias:      whether the attention matrices (Q, K, V) use a bias or not (default: True)
+
+        """
+        super().__init__()
+
+        self.out_dim = out_dim
+        self.sqrt_out_dim = torch.sqrt(torch.tensor(out_dim))
+
+        self.Q = nn.Linear(in_dim, out_dim * num_heads, bias=bias)
+        self.K = nn.Linear(in_dim, out_dim * num_heads, bias=bias)
+        self.V = nn.Linear(in_dim, out_dim * num_heads, bias=bias)
+
+        self.softmax = nn.Softmax(dim=-2)
+        self.phi = phi
+
+
+    def forward(self, h, A, lengths, alpha):
+        """
+        Computes an adjacency aware multi head attention forward pass.
+
+        Args:
+          h:     node embeddings [batch, nodes, in_dim] or [nodes, in_dim]
+          A:     matrix provided to the weighting function [batch, num_nodes, num_nodes] or [num_nodes, num_nodes]
+          lengths: [batch,]
+          alpha: value used by the weighting function
+
+        """
+        if len(h.shape) == 2:
+            h.unsqueeze(0)
+            A.unsqueeze(0)
+
+        assert len(h.shape) == 3, f"AdjacencyAwareMultiHeadAttention Error: Got invalid shape for h {h.shape}!"
+
+        # apply the query, key and value matrices
+        Q_h = self.Q(h)
+        K_h = self.K(h)
+        V_h = self.V(h)
+        
+        Q_h = einops.rearrange(Q_h, 'b n (k d) -> b k n d', d=self.out_dim)
+        K_h = einops.rearrange(K_h, 'b n (k d) -> b k n d', d=self.out_dim)
+        V_h = einops.rearrange(V_h, 'b n (k d) -> b k n d', d=self.out_dim)
+        # lengths = lengths.view((-1,1,1))
+        # Q_h[:,:,lengths:,:] = float('-inf')
+        # K_h[:,:,lengths:,:] = float('-inf')
+        # V_h[:,:,lengths:,:] = float('-inf')
+        # compute the dot products
+        attention = torch.matmul(Q_h, K_h.transpose(-1,-2)).transpose(-1,-2)
+        #Optimize
+        for i in range(lengths.shape[0]):
+            attention[i,:,lengths[i]:] = float('-inf')
+        # apply scaling and softmax to attention
+        attention = self.softmax(attention / self.sqrt_out_dim)
+        for i in range(lengths.shape[0]):
+            attention[i,:,:,lengths[i]:] = 0
+
+        # reweight using the adjacency information
+        attention = attention.moveaxis(1,0)
+        attention = self.phi(attention.transpose(-1,-2), A, alpha)
+        attention = attention.moveaxis(0,1)
 
         # sum value tensors scaled by the attention weights
         h_heads = torch.matmul(attention, V_h)
@@ -160,9 +237,110 @@ class GTA3Layer(nn.Module):
         if len(h.shape) == 3: h = einops.rearrange(h, 'k n d -> n (k d)', d=self.out_dim)
         else:                 h = einops.rearrange(h, 'b k n d -> b n (k d)', d=self.out_dim)
 
+        print("Embeddings after attention in original model")
+        print(f"Embedding sum: {h.sum()}")
         # apply the O matrix
         h = self.O(h)
+        # residual & normalization
+        if self.residual_heads:
+            h = h_in + h
+        h = h.unsqueeze(0)
+        if self.batch_norm:
+            h = self.batch_norm_1(h) # TODO: check that this is correct
+        if self.layer_norm:
+            h = self.layer_norm_1(h)
+        h = h.squeeze(0)
 
+        # feed forward network
+        h_tmp = h
+        h = self.FFN_layer_1(h)
+        h = F.relu(h)
+        h = F.dropout(h)
+        h = self.FFN_layer_2(h)
+
+        # residual & normalization
+        if self.residual_ffn:
+            h = h_tmp + h
+        if self.batch_norm:
+            h = self.batch_norm_2(h) # TODO: check that this is correct
+        if self.layer_norm:
+            h = self.layer_norm_2(h)
+
+        return h
+    
+class GTA3LayerBatched(nn.Module):
+
+    def __init__(self, in_dim, out_dim, phi, num_heads=8, residual=True, batch_norm=False, layer_norm=True, attention_bias=True):
+        """
+        Graph Transformer with Adjacency Aware Attention (GTA3) layer.
+
+        Args:
+          in_dim:         the dimension of the input vectors
+          out_dim:        the dimension of the output vectors
+          phi:            the weighting function to be used
+          num_heads:      the number of attention heads to be used (default: 8)
+          residual:       whether to use residual connections (default: True) (note that if in_dim != out_dim the heads will not have a residual connection)
+          batch_norm:     whether to use batch normalization (default: False)
+          layer_norm:     whether to use layer normalization (default: True)
+          attention_bias: whether the attention matrices (Q, K, V) use a bias or not (default: True)
+
+        """
+        super().__init__()
+
+        self.out_dim = out_dim
+        self.batch_norm = batch_norm
+        self.layer_norm = layer_norm
+        self.residual_heads = residual and in_dim == out_dim
+        self.residual_ffn = residual
+
+        if phi == 'none':
+            self.phi = phi_no_weighting
+        elif phi == 'test':
+            self.phi = phi_simple_adj_weighting
+        elif phi == 'inverse_hops':
+            self.phi = phi_inverse_hops
+        else:
+            print(f"GTA3 Error: Unknown phi function {phi}! Use one of the following: 'none', 'test'")
+            exit()
+
+        self.O = nn.Linear(out_dim * num_heads, out_dim)
+        self.aa_attention = AdjacencyAwareMultiHeadAttentionBatched(in_dim=in_dim, out_dim=out_dim, phi=self.phi, num_heads=num_heads, bias=attention_bias)
+        self.FFN_layer_1 = nn.Linear(out_dim, out_dim * 2)
+        self.FFN_layer_2 = nn.Linear(out_dim * 2, out_dim)
+
+        if batch_norm:
+            self.batch_norm_1 = nn.BatchNorm1d(out_dim)
+            self.batch_norm_2 = nn.BatchNorm1d(out_dim)
+
+        if layer_norm:
+            self.layer_norm_1 = nn.LayerNorm(out_dim)
+            self.layer_norm_2 = nn.LayerNorm(out_dim)
+
+
+    def forward(self, h, A, lengths, alpha):
+        """
+        Computes a GTA3 forward pass.
+
+        Args:
+          h:     node embeddings [batch, nodes, in_dim] or [nodes, in_dim]
+          A:     matrix provided to the weighting function [batch, num_nodes, num_nodes] or [num_nodes, num_nodes]
+          alpha: value used by the weighting function
+
+        """
+        h_in = h
+
+        # perform multihead attention
+        h = self.aa_attention(h, A, lengths, alpha)
+        if len(h.shape) == 3: h = einops.rearrange(h, 'k n d -> n (k d)', d=self.out_dim)
+        else:                 h = einops.rearrange(h, 'b k n d -> b n (k d)', d=self.out_dim)
+
+        print("Embeddings after attention in batched model")
+        for i,h_i in enumerate(h):
+            print(f"Embedding sum {i}: {h_i.sum()}")
+
+        # apply the O matrix
+        h = self.O(h)
+        
         # residual & normalization
         if self.residual_heads:
             h = h_in + h
@@ -170,7 +348,7 @@ class GTA3Layer(nn.Module):
             h = self.batch_norm_1(h) # TODO: check that this is correct
         if self.layer_norm:
             h = self.layer_norm_1(h)
-
+        
         # feed forward network
         h_tmp = h
         h = self.FFN_layer_1(h)
@@ -240,6 +418,75 @@ class GTA3BaseModel(L.LightningModule):
         )
         self.gta3_layers.append(
             GTA3Layer(
+                in_dim=model_params['hidden_dim'], out_dim=model_params['out_dim'], num_heads=model_params['num_heads'], phi=model_params['phi'],
+                residual=model_params['residual'], batch_norm=model_params['batch_norm'], layer_norm=model_params['layer_norm'],
+                attention_bias=model_params['attention_bias'])
+        )
+
+
+    def training_step(self, batch, batch_idx):
+        raise NotImplementedError("GTA3BaseModel: Define training_step function!")
+
+
+    def validation_step(self, batch, batch_idx):
+        raise NotImplementedError("GTA3BaseModel: Define training_step function!")
+
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=self.train_params['lr'])
+        return optimizer
+    
+
+class GTA3BaseModelBatched(L.LightningModule):
+    
+    def __init__(self, model_params, train_params):
+        """
+        Graph Transformer with Adjacency Aware Attention (GTA3) base model.
+
+        Args:
+          model_params: dictionary containing the model parameters
+          train_params: dictionary containing the training parameters
+
+        """
+        super().__init__()
+
+        self.train_params = train_params
+
+        # initialize the alpha value
+        if model_params['alpha'] == 'fixed':
+            self.per_layer_alpha = False
+            self.alpha = torch.tensor([model_params['alpha_init']], dtype=torch.float)
+        elif model_params['alpha'] == 'per_model': # TODO: test this...
+            self.per_layer_alpha = False
+            self.alpha = torch.nn.Parameter(torch.tensor([model_params['alpha_init']], dtype=torch.float))
+        elif model_params['alpha'] == 'per_layer': # TODO: test this...
+            self.per_layer_alpha = True
+            if type(model_params['alpha_init']) == list:
+                assert len(model_params['alpha_init']) == model_params['num_layers'], \
+                    f"Number of layers ({model_params['num_layers']}) does not match number of initial alpha values given ({len(model_params['alpha_init'])})!"
+                self.alpha = torch.nn.Parameter(torch.tensor(model_params['alpha_init'], dtype=torch.float))
+            else:
+                self.alpha = torch.nn.Parameter(torch.tensor([model_params['alpha_init'] for _ in range(model_params['num_layers'])], dtype=torch.float))
+        elif model_params['alpha'] == 'per_head': # TODO: test this...
+            self.per_layer_alpha = True
+            # TODO: implement
+            raise NotImplementedError("alpha per head is not yet implemented...")
+        else:
+            raise ValueError("Invalid alpha model parameter!", model_params['alpha'])
+
+        # creates an embedding depending on the node type
+        self.embedding = nn.Embedding(model_params['num_types'], model_params['hidden_dim'])
+        
+        # the main part of the model
+        self.gta3_layers = nn.ModuleList(
+            [ GTA3LayerBatched(
+                in_dim=model_params['hidden_dim'], out_dim=model_params['hidden_dim'], num_heads=model_params['num_heads'], phi=model_params['phi'],
+                residual=model_params['residual'], batch_norm=model_params['batch_norm'], layer_norm=model_params['layer_norm'], 
+                attention_bias=model_params['attention_bias']) 
+            for _ in range(model_params['num_layers']-1) ]
+        )
+        self.gta3_layers.append(
+            GTA3LayerBatched(
                 in_dim=model_params['hidden_dim'], out_dim=model_params['out_dim'], num_heads=model_params['num_heads'], phi=model_params['phi'],
                 residual=model_params['residual'], batch_norm=model_params['batch_norm'], layer_norm=model_params['layer_norm'],
                 attention_bias=model_params['attention_bias'])
--- zinc/zinc_gta3.py
+++ zinc/zinc_gta3.py
@@ -4,7 +4,7 @@ from dgl import save_graphs, load_graphs
 from dgl.data import ZINCDataset
 from dgl.data.utils import save_info, load_info
 
-from gta3.model import GTA3BaseModel
+from gta3.model import GTA3BaseModel, GTA3BaseModelBatched
 from gta3.dataloader import GTA3BaseDataset
 from gta3.loss import L1Loss_L1Alpha, L1Loss_L2Alpha
 
@@ -114,7 +114,100 @@ class GTA3_ZINC(GTA3BaseModel):
 
     def training_step(self, batch, batch_idx):
         x, A, y_true = batch
+        print(x)
+        print(A)
+        # forward pass
+        y_pred = self.forward_step(x, A)
+        
+        # compute loss
+        if self.train_alpha:
+            train_loss = self.train_loss_func(y_pred, y_true, self.alpha, self.alpha_weight) # NOTE: might not yet work for per head alpha
+        else:
+            train_loss = self.train_loss_func(y_pred, y_true)
 
+        # log loss and alpha
+        if self.per_layer_alpha:
+            for l in range(len(self.gta3_layers)):
+                self.log(f"alpha/alpha_{l}", self.alpha[l], on_epoch=False, on_step=True, batch_size=1)
+        else:
+            self.log("alpha/alpha_0", self.alpha, on_epoch=False, on_step=True, batch_size=1)
+        self.log("train_loss", train_loss, on_epoch=True, on_step=False, batch_size=1)
+
+        return train_loss
+    
+
+    def validation_step(self, batch, batch_idx):
+        x, A, y_true = batch
+
+        # forward pass
+        y_pred = self.forward_step(x, A)
+
+        # compute loss
+        valid_loss = self.valid_loss_func(y_pred, y_true)
+
+        # log loss
+        self.log("valid_loss", valid_loss, on_epoch=True, on_step=False, batch_size=1)
+
+        return valid_loss
+    
+
+class GTA3_ZINC_Batched(GTA3BaseModelBatched):
+    
+    def __init__(self, model_params, train_params):
+        
+        # initialize the GTA3 base model
+        super().__init__(model_params, train_params)
+
+        # final mlp to map the out dimension to a single value
+        self.out_mlp = nn.Sequential(nn.Linear(model_params['out_dim'], model_params['out_dim'] * 2), nn.ReLU(), nn.Dropout(), nn.Linear(model_params['out_dim'] * 2, 1))
+        # self.out_mlp = nn.Sequential(
+        #     nn.Linear(model_params['out_dim'], model_params['out_dim'] * 2),
+        #     nn.ReLU(), 
+        #     # nn.Dropout(), 
+        #     nn.Linear(model_params['out_dim'] * 2, model_params['out_dim'] // 2),
+        #     nn.ReLU(), 
+        #     # nn.Dropout(), 
+        #     nn.Linear(model_params['out_dim'] // 2, 1),
+        # )
+        
+        # loss functions
+        if model_params['alpha'] == 'fixed':
+            self.train_alpha = False
+            self.train_loss_func = nn.L1Loss()
+        else:
+            self.train_alpha = True
+            self.train_loss_func = L1Loss_L1Alpha
+            self.alpha_weight = model_params['alpha_weight']
+        self.valid_loss_func = nn.L1Loss()
+
+
+    def forward_step(self, x, A, lengths):
+        # x: [B, N]
+        # A: [B, N, Emb]
+        # lengths: [B]
+        self.alpha = self.alpha.to(device=self.device)
+
+        # create embeddings
+        h = self.embedding(x)
+
+        # pass through transformer layers
+        for idx, layer in enumerate(self.gta3_layers):
+            if self.per_layer_alpha: 
+                h = layer.forward(h, A, lengths, self.alpha[idx])
+            else:
+                h = layer.forward(h, A, lengths, self.alpha)
+
+        # combine resulting node embeddings
+        h = torch.mean(h, dim=-2) # TODO: using mean for now
+
+        # pass through final mlp
+        return self.out_mlp(h)
+
+
+    def training_step(self, batch, batch_idx):
+        x, A, y_true = batch
+        print(x)
+        print(A)
         # forward pass
         y_pred = self.forward_step(x, A)
         
