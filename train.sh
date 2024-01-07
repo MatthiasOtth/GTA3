72fb84e6019fe3dcb50697210a517b89fe65e189
ThomasRupf <tm.rupf@gmail.com>
2024-01-07 15:50:46 +0100

    runs

--- train.sh
+++ train.sh
@@ -29,7 +29,7 @@ source .venv/bin/activate
 echo "python: $(which python3)"
 echo "python version: $(python3 --version)"
 
-srun python3 cluster_main.py config/cluster/gta3_500k.yaml
+srun python3 cluster_main.py config/cluster/gta3_500k.json
 
 cd --
 
