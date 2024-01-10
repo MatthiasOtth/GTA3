78f167322ece618b686b885010b22af5d99ac51b
Tobias Stocker <tstocker@student-cluster.inf.ethz.ch>
2024-01-10 14:33:06 +0100

    Changed max runtime to 4h

--- nbm_main.py
+++ nbm_main.py
@@ -87,7 +87,7 @@ def main():
         logger = None
     trainer = L.Trainer(
         #max_epochs=config['train_params']['max_epochs'], 
-        max_time='00:12:00:00',
+        max_time='00:04:00:00',
         logger=logger, 
         check_val_every_n_epoch=1,  # needed for lr scheduler
         callbacks=[StopOnLrCallback(lr_threshold=config['train_params']['lr_threshold'], on_val=True), StopOnValAcc(acc_thresh=1.0, on_val=True)],
