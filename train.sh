28ea5a202ef36c4a92a6f828f3dee6b087a02e48
ThomasRupf <tm.rupf@gmail.com>
2024-01-10 11:09:42 +0100

    runs

--- train.sh
+++ train.sh
@@ -30,6 +30,7 @@ echo "python: $(which python3)"
 echo "python version: $(python3 --version)"
 
 srun python3 cluster_main.py config/cluster/gta3_500k_shallow.json
+srun python3 zinc_main.py config/zinc/gta3_500k_shallow.json
 
 cd --
 
