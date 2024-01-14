0316b32ab595fb47541f5e1ff3958df6fef32fb6
ThomasRupf <tm.rupf@gmail.com>
2024-01-14 15:49:11 +0100

    fixes bug in cluster acc calc

--- cluster/cluster_gta3.py
+++ cluster/cluster_gta3.py
@@ -164,7 +164,7 @@ class GTA3_CLUSTER(GTA3BaseModel):
         accuracy = (preds == labels).sum().float() / total
 
         # log accuracy
-        self.log(self.score_name, accuracy, on_epoch=True, on_step=False, batch_size=batch_size, prog_bar=True)
+        self.log(self.score_name, accuracy, on_epoch=True, on_step=False, batch_size=total, prog_bar=True)
 
         return accuracy
 
@@ -182,7 +182,7 @@ class GTA3_CLUSTER(GTA3BaseModel):
         accuracy = (preds == labels).sum().float() / total
 
         # log accuracy
-        self.log("test_accuracy", accuracy, on_epoch=True, on_step=False, batch_size=batch_size)
+        self.log("test_accuracy", accuracy, on_epoch=True, on_step=False, batch_size=total)
 
         return accuracy
     
